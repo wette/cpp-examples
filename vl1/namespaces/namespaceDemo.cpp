241e7053a5e0d2563f726ef0c36216c0ba52fb89
Philip Wette <philip.wette@fh-bielefeld.de>
2022-08-17 13:51:22 +0200

    extended example

--- vl1/namespaces/namespaceDemo.cpp
+++ vl1/namespaces/namespaceDemo.cpp
@@ -3,17 +3,27 @@
 
 //eigenen Namespace definieren
 namespace XYZ {
+    //eigene Funktion in eigenem Namespace definieren
     void meineFunktion() {
-        //tut nichts.
+        std::cout << "Funktion aus Namespace XYZ aufgerufen." << std::endl;
     }
 }
 
+//eigene Funktion ohne Namespace definieren (--> lebt im default namespace)
+void meineFunktion() {
+    std::cout << "Funktion im default Namespace aufgerufen." << std::endl;
+}
+
+
+// es ist auch möglich, Definitionen aus beliebigen Namespaces 
+// in den default Namespace zu übernehmen:
 //using namespace std;
 int main() {
     std::string myString = "lalala";
     myString += "blabla";
 
     XYZ::meineFunktion();
+    meineFunktion();
 
     std::cout << myString << std::endl;
 }
